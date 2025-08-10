93dc26172a3068808626ff24a3b0e1d2c6edc0a4
T1mothy Nj4u <93652350+TimothyNjau@users.noreply.github.com>
2025-08-10 23:27:46 +0300

    Implement layout components and enhance styling for the movie app

--- alx-movie-app/components/layout/Layout.tsx
+++ alx-movie-app/components/layout/Layout.tsx
@@ -1,10 +1,16 @@
+import { ComponentProps } from "&/interfaces";
+import Header from "./Header";
+import Footer from "./Footer";
 
-
-const Layout : React.FC = () => {
+const Layout : React.FC<ComponentProps> = ({children}) => {
 
     return (
         <>
-        
+        <Header />
+        <main className="min-h-screen">
+            {children}
+        </main>
+        <Footer />
         </>
     )
 }
